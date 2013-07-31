9388c6643fa3b9e69751f36f729f02a08805bb78
Ed Hynan <freecode@verizon.net>
2013-07-31 11:39:18 -0400

    Update version.sh for new branch DEV_0_0_4_3.

--- version.sh
+++ version.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-V="0.0.4.2"
+V="0.0.4.3"
 FMT='%s\n'
 test X"$1" = X-s && FMT='"%s"\n'
-test X"$1" = X-i && V="1026"
+# 32 bit int: vmajor<<24|vminor<<16|rmajor<<8|rminor
+test X"$1" = X-i && V="1027"
 printf "$FMT" "$V"
